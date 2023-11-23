61fd96a6be8be0e7db01a24fa4808dd4b3a4ac86
Trong <jr16022002@gmail.com>
2023-11-23 10:42:51 +0700

    Form Đăng ký

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
 
+export class RegisterComponent {
+  public registerForm: FormGroup = new FormGroup({
+
+  });
+  public register (): void
+  {
+
+  }
 }

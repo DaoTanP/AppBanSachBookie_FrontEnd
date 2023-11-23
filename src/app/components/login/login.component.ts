201c827affb0005b49a1413cba4457e4274c9153
Hieu <hieuvo@gmail.com>
2023-11-23 10:38:32 +0700

    Form Đăng Nhập

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent
+{
+  protected waiting: boolean = false;
 
+  protected username: FormControl = new FormControl(null, [Validators.required, Validators.minLength(5)]);
+  protected password: FormControl = new FormControl(null, [Validators.required, Validators.minLength(8)]);
+
+  public loginForm: FormGroup = new FormGroup({
+    username: this.username,
+    password: this.password,
+  });
+  constructor()
+  {
+    
+  }
+
+  public login (): void
+  {
+    
+  }
 }
